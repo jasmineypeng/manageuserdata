685018572f5151f7687960f7cb67518bd81d539f
Jasmine <jaasminepeng@gmail.com>
2024-03-08 12:55:30 -0800

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,15 @@
+/**
+* Assignment: <Lab 1: Manage User Accounts> 
+* Name: <Jasmine Peng>
+* Description: In this program, we are creating an instance of the UserMenu class and having the class run once with a simple call to activate.
+* Date: <2/24/2024>
+*/
+
+#include "userMenu.h"
+using namespace std;
+
+int main() {
+    UserMenu app;    // invoke the default constructor
+    app.activate();    // Start the user menu selection
+    return 0;
+}
